9a27cf75eb9b92ae6d211f1a127131e39e0a8fb7
Bryan BESSAUD <bryan.bessaud@ingenico.com>
2019-07-04 16:24:00 +0200

    Added circle and seekbar on Map. New issues and and things to improve. Moving on to searching for places.

--- app/src/main/java/com/example/bbessaud/letsgo/MapsActivity.java
+++ app/src/main/java/com/example/bbessaud/letsgo/MapsActivity.java
@@ -3,10 +3,13 @@ package com.example.bbessaud.letsgo;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.location.Location;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v7.widget.AppCompatSeekBar;
+import android.widget.SeekBar;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -14,14 +17,19 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.UiSettings;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.Circle;
+import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
+    private LatLng user;
     private GoogleMap mMap;
+    private Circle circle;
     private UiSettings mUiSettings;
     private Location mLocation;
+    private float zoomLevel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,10 +44,31 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             }
         }
 
+        user = new LatLng(mLocation.getLatitude(), mLocation.getLongitude());
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        SeekBar circleRadius = findViewById(R.id.radiusSeekBar);
+        circleRadius.setMax(99);
+        circleRadius.setProgress(0);
+
+        circleRadius.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                circle.setRadius(50*progress + 50);
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(user, getZoomLevel(circle)));
+            }
+
+            @Override
+            public void onStartTrackingTouch(final SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(final SeekBar seekBar) {
+            }
+        });
     }
 
 
@@ -74,12 +103,29 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mUiSettings.setCompassEnabled(true); //TODO compass does not show up
 
         // Add a marker
-        LatLng user = new LatLng(mLocation.getLatitude(), mLocation.getLongitude());
         mMap.addMarker(new MarkerOptions()
                 .position(user)
                 .visible(false)
         );
 
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(user, 15));
+        // Add its circle
+        circle = mMap.addCircle(new CircleOptions()
+                .center(user)
+                .fillColor(Color.BLUE)
+                .strokeColor(Color.WHITE)
+                .strokeWidth(0.5f)
+                .radius(50)
+        );
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(user, getZoomLevel(circle)));
+    }
+
+    public float getZoomLevel(Circle circle) {
+        if (circle != null){
+            float radius = (float) circle.getRadius();
+            float scale = radius / 500;
+            zoomLevel = (float) (14 - Math.log(scale) / Math.log(2.0));
+        }
+        return zoomLevel;
     }
 }
