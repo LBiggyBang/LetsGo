0dee9db2058b4ae42de68da89e8c07186877dfc7
Bryan BESSAUD <bryan.bessaud@ingenico.com>
2019-07-05 17:12:43 +0200

    Added the destination type buttons. Prepared next activity, but must retrieve the right distance from map activity before implementing the rest.

--- app/src/main/java/com/example/bbessaud/letsgo/MainActivity.java
+++ app/src/main/java/com/example/bbessaud/letsgo/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.bbessaud.letsgo;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -12,6 +13,7 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -20,11 +22,19 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
 
     private static final int PERMISSIONS_REQUEST_LOCATION = 1234;
 
+    private Button eatButton;
+    private Button drinkButton;
+    private Button activityButton;
+
     private Context mContext;
     private LocationManager mLocationManager;
     protected Location mLocation;
     private boolean isGpsStarted = false;
 
+    public String destinationType;
+    public int searchDistance = 50;
+
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,16 +47,76 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
         }
         setContentView(R.layout.activity_main);
 
-        final TextView textView = findViewById(R.id.location);
-        Button button = findViewById(R.id.getLocation);
-        button.setOnClickListener(new View.OnClickListener() {
+        final TextView radiusTextView = findViewById(R.id.distanceTextView);
+
+        // Activity type buttons
+        eatButton = findViewById(R.id.eatButton);
+        drinkButton = findViewById(R.id.drinkButton);
+        activityButton = findViewById(R.id.activityButton);
+        eatButton.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                v.setPressed(true);
+                drinkButton.setPressed(false);
+                activityButton.setPressed(false);
+
+                destinationType = "eat";
+                return true;
+            }
+        });
+        drinkButton.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                v.setPressed(true);
+                eatButton.setPressed(false);
+                activityButton.setPressed(false);
+
+                destinationType = "drink";
+                return true;
+            }
+        });
+        activityButton.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                v.setPressed(true);
+                eatButton.setPressed(false);
+                drinkButton.setPressed(false);
+
+                destinationType = "activity";
+                return true;
+            }
+        });
+
+        // Get radius button
+        Button getRadiusButton = findViewById(R.id.getRadiusButton);
+        getRadiusButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.i("MainActivity", "Map button clicked, going to map activity");
+                Log.i("MainActivity", "Get radius button clicked, going to map activity");
                 Intent mapIntent = new Intent(MainActivity.this, MapsActivity.class);
                 mapIntent.putExtra("userLocation", mLocation);
 
                 startActivity(mapIntent);
+            }
+        });
+
+        // Search button
+        Button searchButton = findViewById(R.id.searchButton);
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (null != destinationType && mLocation != null){
+                    Log.i("MainActivity", "Search button clicked, going to search activity");
+                    Intent searchIntent = new Intent(MainActivity.this, SearchActivity.class);
+
+                    Bundle searchExtras = new Bundle();
+                    searchExtras.putString("destinationType", destinationType);
+                    searchExtras.putInt("searchDistance", searchDistance);
+                    searchExtras.putParcelable("userLocation", mLocation);
+                    searchIntent.putExtras(searchExtras);
+
+                    startActivity(searchIntent);
+                }
             }
         });
     }
