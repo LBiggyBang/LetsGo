2bf4167ab459b97a08b1f8b61de7c2e88b04f929
Bryan BESSAUD <bryan.bessaud@ingenico.com>
2019-07-08 17:01:49 +0200

    Found how to make a Places Search request. Must now implement HttpConnection and an AsyncTask, before parsing a response.

--- app/src/main/java/com/example/bbessaud/letsgo/SearchActivity.java
+++ app/src/main/java/com/example/bbessaud/letsgo/SearchActivity.java
@@ -3,11 +3,37 @@ package com.example.bbessaud.letsgo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.io.BufferedInputStream;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+
 public class SearchActivity extends AppCompatActivity {
+
+    private ArrayList<String> placesList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+
+        // Retrieve search info and create a search URL
+
+        // Request a places search
+        URL url = new URL("http://www.android.com/");
+        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+        try {
+            InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+        } finally {
+            urlConnection.disconnect();
+        }
+
+        // Create
+
+    }
+
+    private void generateRandomDestination (String response){
+
     }
 }
